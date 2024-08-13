fd18d2524ec1ffad9ea4e673a253fe50a0b6bdb5
Ian Ferguson <iansterlingferguson@gmail.com>
2024-08-12 17:01:48 -0700

    working through errors.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,53 +1,51 @@
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 const router = Router();
 
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', async (req, res) => {
-  const cityName = req.body.city;
+router.post('/', async (req: Request, res: Response) => {
+  const cityName = req.body.cityName;
 
   if (!cityName) {
-    res.status(400).json({ msg: 'City name is required' });
+    return res.status(400).json({ msg: 'City name is required' });
   }
 
   // TODO: GET weather data from city name
   try {
     const weatherData = await WeatherService.getWeatherForCity(cityName);
-    
+
     // TODO: save city to search history
     await HistoryService.addCity(cityName);
 
-    res.json(weatherData);
+    return res.json(weatherData);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
 // TODO: GET search history
-router.get('/history', async (_req, res) => {
-  try {
+router.get('/history', async (_req: Request, res: Response) => {  try {
     const savedCities = await HistoryService.getCities();
-    res.json(savedCities);
+    return res.json(savedCities);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {
-  try {
+router.delete('/history/:id', async (req: Request, res: Response) => {  try {
     if (!req.params.id) {
-      res.status(400).json({ msg: 'City id is required' });
+      return res.status(400).json({ msg: 'City id is required' });
     }
     await HistoryService.removeCity(req.params.id);
-    res.json({ success: 'City successfully removed from search history'});
+    return res.json({ success: 'City successfully removed from search history'});
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -43,12 +43,12 @@ class WeatherService {
 
   private apiKey?: string;
 
-  private city?: string;
+  private cityName?: string;
 
   constructor() {
-    this.baseURL = process.env.WEATHER_API_BASE_URL || '';
-    this.apiKey = process.env.WEATHER_API_KEY || '';
-    this.city = '';
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.apiKey = process.env.API_KEY || '';
+    this.cityName = '';
   }
 
   // TODO: Create fetchLocationData method
@@ -74,19 +74,19 @@ class WeatherService {
 
   // TODO: Create buildGeocodeQuery method
   private buildGeocodeQuery(): string {
-    return `${this.baseURL}/geo/1.0/direct?q=${this.city}&limit=1&appid=${this.apiKey}`;
+    return `${this.baseURL}/geo/1.0/direct?q=${this.cityName}&limit=1&appid=${this.apiKey}`;
   }
 
   // TODO: Create buildWeatherQuery method
   private buildWeatherQuery(coordinates: Coordinates): string {
-    return `${this.baseURL}/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=minutely&appid=${this.apiKey}`;
+    return `${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.apiKey}`;
   }
 
   // TODO: Create fetchAndDestructureLocationData method
   private async fetchAndDestructureLocationData() {
     try {
       const locationData = await this.fetchLocationData(this.buildGeocodeQuery());
-      return this.destructureLocationData(locationData[0]);
+      return this.destructureLocationData(locationData);
     } catch (err) {
       console.log('Error: ', err);
       return err;
@@ -100,6 +100,7 @@ class WeatherService {
       );
 
       const weatherData = await response.json();
+      console.log('Weather Data: ', weatherData);
       return weatherData;
     } catch (err) {
       console.log('Error: ', err);
@@ -110,17 +111,13 @@ class WeatherService {
   // TODO: Build parseCurrentWeather method
   private parseCurrentWeather(response: any) {
     return new Weather(
-      this.city!,
-      new Date(response.current.dt * 1000).toLocaleDateString('en-US', {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-      }),
-      response.current.temp,
-      response.current.weather[0].description,
-      response.current.weather[0].icon,
-      response.current.humidity,
-      response.current.wind_speed
+      response.city.name,
+      response.list[0].dt_txt,
+      response.list[0].main.temp,
+      response.list[0].weather[0].description,
+      response.list[0].weather[0].icon,
+      response.list[0].main.humidity,
+      response.list[0].wind.speed
     );
   }
 
@@ -129,16 +126,12 @@ class WeatherService {
     const forecastArray: Weather[] = weatherData.map((instance) => {
       return new Weather (
         currentWeather.cityName,
-        new Date(instance.dt * 1000).toLocaleDateString('en-US', {
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric',
-        }),
-        instance.temp.day,
+        instance.dt_txt,
+        instance.main.temp,
         instance.weather[0].description,
         instance.weather[0].icon,
-        instance.humidity,
-        instance.wind_speed
+        instance.main.humidity,
+        instance.wind.speed
       );
     });
     return forecastArray;
@@ -147,7 +140,7 @@ class WeatherService {
   // TODO: Complete getWeatherForCity method
   async getWeatherForCity(city: string) {
     try {
-      this.city = city;
+      this.cityName = city;
 
       const coordinates = await this.fetchAndDestructureLocationData() as Coordinates;
 
@@ -155,9 +148,9 @@ class WeatherService {
 
       const currentWeather = this.parseCurrentWeather(weatherData);
 
-      const forecast = this.buildForecastArray(currentWeather, weatherData.daily);
+      const forecast = this.buildForecastArray(currentWeather, weatherData.list);
 
-      return forecast;
+      return [currentWeather, ...forecast];
     } catch (err) {
       console.log('Error: ', err);
       return err;
